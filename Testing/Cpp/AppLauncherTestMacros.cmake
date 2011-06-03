941da8d2fc0b5652769e8b48ed5b0ca4a0914b3f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-06-03 00:56:21 -0400

    COMP: Factor out test prerequisites and test macros

---
+++ Testing/Cpp/AppLauncherTestMacros.cmake
@@ -0,0 +1,12 @@
+
+# --------------------------------------------------------------------------
+# Helper macro
+FUNCTION(print_command_as_string command)
+  if(PRINT_COMMAND)
+    set(command_as_string)
+    foreach(elem ${command})
+      set(command_as_string "${command_as_string} ${elem}")
+    endforeach()
+    message(STATUS "COMMAND:${command_as_string}")
+  endif()
+ENDFUNCTION()
