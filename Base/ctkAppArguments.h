f8276ce189e3b48b38c23931b7e4d900411dea12
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-03-27 03:44:27 -0400

    Arguments specific to Qt are not "eaten" anymore. See #2
    * Added convenience classes "ctkChar2DArray" and "ctkAppArguments"
    
    * "ctkAppArguments" is full unit tested

---
+++ Base/ctkAppArguments.h
@@ -0,0 +1,91 @@
+#ifndef __ctkAppArguments_h
+#define __ctkAppArguments_h
+
+// Qt includes
+#include <QPair>
+#include <QScopedPointer>
+
+// --------------------------------------------------------------------------
+class ctkChar2DArrayPrivate;
+class QStringList;
+
+// --------------------------------------------------------------------------
+class ctkChar2DArray
+{
+public:
+  ctkChar2DArray();
+  ctkChar2DArray(const QStringList& list);
+  virtual ~ctkChar2DArray();
+  void setValues(const QStringList& list);
+  void clear();
+  char** values()const;
+  int& count();
+protected:
+  QScopedPointer<ctkChar2DArrayPrivate> d_ptr;
+
+private:
+  Q_DECLARE_PRIVATE(ctkChar2DArray);
+  Q_DISABLE_COPY(ctkChar2DArray);
+};
+
+// --------------------------------------------------------------------------
+class ctkAppArgumentsPrivate;
+template <typename T> class QList;
+
+// --------------------------------------------------------------------------
+class ctkAppArguments
+{
+public:
+  typedef ctkAppArguments Self;
+  ctkAppArguments(int& argc, char* argv[]);
+  ~ctkAppArguments();
+
+  enum ArgListType
+    {
+    ARG_RESERVED_LIST = 0x1,
+    ARG_REGULAR_LIST = 0x2,
+    ARG_FULL_LIST = ARG_RESERVED_LIST & ARG_REGULAR_LIST
+    };
+  Q_DECLARE_FLAGS(ArgListTypes, ArgListType)
+
+  QStringList arguments(ArgListTypes option = ARG_FULL_LIST)const;
+
+  char** argumentValues(ArgListTypes option = ARG_FULL_LIST) const;
+
+  int& argumentCount(ArgListTypes option = ARG_FULL_LIST);
+
+  enum ArgToFilterOption
+    {
+    ARG_TO_FILTER_NO_VALUE = 0x0,
+    ARG_TO_FILTER_EQUAL_VALUE = 0x1,
+    ARG_TO_FILTER_SPACE_VALUE = 0x2
+    };
+  Q_DECLARE_FLAGS(ArgToFilterOptions, ArgToFilterOption)
+
+  class ArgToFilterType : public QPair<QString, ArgToFilterOptions>
+  {
+  public:
+    typedef QPair<QString, ctkAppArguments::ArgToFilterOptions> Superclass;
+    ArgToFilterType():Superclass(){}
+    ArgToFilterType(const QString& arg, ctkAppArguments::ArgToFilterOptions options = ARG_TO_FILTER_NO_VALUE) :
+      Superclass(arg, options){}
+  };
+  typedef QList< ArgToFilterType > ArgToFilterListType;
+
+  void addArgumentToFilter(const ArgToFilterType& argToFilter);
+
+  ArgToFilterListType argumentToFilterList()const;
+
+  void setArgumentToFilterList(const ArgToFilterListType& list);
+
+protected:
+  QScopedPointer<ctkAppArgumentsPrivate> d_ptr;
+
+private:
+  Q_DECLARE_PRIVATE(ctkAppArguments);
+  Q_DISABLE_COPY(ctkAppArguments);
+};
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(ctkAppArguments::ArgToFilterOptions);
+
+#endif
--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,14 @@
 
 // Qt includes
 #include <QApplication>
+#include <QStringList>
 #include <QTimer>
 #include <QDebug>
 
 // STD includes
 #include <cstdlib>
 
+#include "ctkAppArguments.h"
 #include "ctkAppLauncher.h"
 
 int main(int argc, char** argv)
@@ -16,13 +18,56 @@ int main(int argc, char** argv)
   // Setting the application to be a plugin will avoid the loading of qt_menu.nib files
   QCoreApplication::setAttribute(Qt::AA_MacPluginApplication, true);
   #endif
-  QApplication app(argc, argv);
-  
+
+  ctkAppArguments appArguments(argc, argv);
+
+  // See http://qt-project.org/doc/qt-4.8/qapplication.html#QApplication
+  appArguments.setArgumentToFilterList(
+        ctkAppArguments::ArgToFilterListType()
+        << ctkAppArguments::ArgToFilterType("-style", ctkAppArguments::ARG_TO_FILTER_EQUAL_VALUE | ctkAppArguments::ARG_TO_FILTER_SPACE_VALUE)
+        << ctkAppArguments::ArgToFilterType("-stylesheet", ctkAppArguments::ARG_TO_FILTER_EQUAL_VALUE | ctkAppArguments::ARG_TO_FILTER_SPACE_VALUE)
+        << ctkAppArguments::ArgToFilterType("-session", ctkAppArguments::ARG_TO_FILTER_EQUAL_VALUE | ctkAppArguments::ARG_TO_FILTER_SPACE_VALUE)
+        << ctkAppArguments::ArgToFilterType("-widgetcount")
+        << ctkAppArguments::ArgToFilterType("-reverse")
+        << ctkAppArguments::ArgToFilterType("-graphicssystem")
+        << ctkAppArguments::ArgToFilterType("-qmljsdebugger=", ctkAppArguments::ARG_TO_FILTER_EQUAL_VALUE)
+#ifdef QT_DEBUG
+        << ctkAppArguments::ArgToFilterType("-nograb")
+#endif
+#ifdef Q_WS_X11
+        << ctkAppArguments::ArgToFilterType("-display")
+        << ctkAppArguments::ArgToFilterType("-geometry")
+        << ctkAppArguments::ArgToFilterType("-fn")
+        << ctkAppArguments::ArgToFilterType("-font")
+        << ctkAppArguments::ArgToFilterType("-bg")
+        << ctkAppArguments::ArgToFilterType("-background")
+        << ctkAppArguments::ArgToFilterType("-fg")
+        << ctkAppArguments::ArgToFilterType("-foreground")
+        << ctkAppArguments::ArgToFilterType("-btn")
+        << ctkAppArguments::ArgToFilterType("-button")
+        << ctkAppArguments::ArgToFilterType("-name")
+        << ctkAppArguments::ArgToFilterType("-title")
+        << ctkAppArguments::ArgToFilterType("-visual")
+        << ctkAppArguments::ArgToFilterType("-ncols")
+        << ctkAppArguments::ArgToFilterType("-cmap")
+        << ctkAppArguments::ArgToFilterType("-im")
+        << ctkAppArguments::ArgToFilterType("-inputstyle")
+# ifdef QT_DEBUG
+        << ctkAppArguments::ArgToFilterType("-dograb")
+        << ctkAppArguments::ArgToFilterType("-sync")
+# endif
+#endif
+        );
+
+  QApplication app(appArguments.argumentCount(ctkAppArguments::ARG_REGULAR_LIST),
+                   appArguments.argumentValues(ctkAppArguments::ARG_REGULAR_LIST));
+
   // Initialize resources in static libs
   Q_INIT_RESOURCE(CTKAppLauncherBase);
-  
+
   ctkAppLauncher appLauncher(app);
-  
+  appLauncher.setArguments(appArguments.arguments());
+
   QTimer::singleShot(0, &appLauncher, SLOT(startLauncher()));
 
   return app.exec();
