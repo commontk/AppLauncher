9d709dd8065315bf5ff6b0da6e2c4d157d5666c1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-09-09 10:01:35 -0400

    Improve ctkAppLauncherEnvironment comment
    [ci skip]
    
    Suggested-by: Andras Lasso <lasso@queensu.ca>

--- Base/ctkAppLauncherEnvironment.h
+++ Base/ctkAppLauncherEnvironment.h
@@ -37,7 +37,7 @@ public:
   /// environment variable.
   ///
   /// If the environment variable is not set or set to an invalid
-  /// value, it returns 0.
+  /// value, it returns 0 which corresponds to the system environment.
   static int currentLevel();
 
   /// Return the environment corresponding to the requested \c level.
@@ -48,6 +48,37 @@ public:
   /// using the `APPLAUNCHER_<level>_<varname>` convention.
   ///
   /// The variables matching `APPLAUNCHER_<level+1>_<varname>` are not returned.
+  ///
+  /// For example, assuming the current environment contains the following
+  /// variables:
+  ///
+  /// \code
+  /// APPLAUNCHER_LEVEL=1
+  /// APPLAUNCHER_0_PATH=/path/to/level-0
+  /// APPLAUNCHER_0_FRUIT=apple
+  /// APPLAUNCHER_1_PATH=/path/to/level-1:/path/to/level-0
+  /// APPLAUNCHER_1_FRUIT=banana
+  /// PATH=/path/to/level-2:/path/to/level-1:/path/to/level-0
+  /// FRUIT=kiwi
+  /// \endcode
+  ///
+  /// calling `environment(1)` would return:
+  ///
+  /// \code
+  /// APPLAUNCHER_LEVEL=0
+  /// APPLAUNCHER_0_PATH=/path/to/level-0
+  /// APPLAUNCHER_0_FRUIT=apple
+  /// PATH=/path/to/level-1:/path/to/level-0
+  /// FRUIT=banana
+  /// \endcode
+  ///
+  /// and calling `environment(0)` would return:
+  ///
+  /// \code
+  /// PATH=/path/to/level-0
+  /// FRUIT=apple
+  /// \endcode
+  ///
   static QProcessEnvironment environment(int level);
 
   /// Updated \c env including a saved copy of \c variables found in \c systemEnvironment.
