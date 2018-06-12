342dece6547ca8be32c7d8308677b9a913d79613
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-06-12 16:25:41 -0400

    style: Document ctkAppLauncherPrivate::buildEnvironment() function

--- Base/ctkAppLauncher_p.h
+++ Base/ctkAppLauncher_p.h
@@ -66,6 +66,18 @@ public:
   /// build prepend expressions to environment variables, like \c FOO=...:$FOO.
   static QString shellQuote(bool posix, QString text, const QString& trailing = QString());
 
+  /// \brief Update the current process environment.
+  ///
+  /// Environment variables to set are retrieved using ctkAppLauncherSettings::envVars()
+  /// and ctkAppLauncherSettings::pathsEnvVars().
+  ///
+  /// If the internal variable \c LoadEnvironment was set to value >=0, environment
+  /// values are restored using ctkAppLauncherEnvironment::environment(int requestedLevel).
+  ///
+  /// Otherwise, the environment is saved using ctkAppLauncherEnvironment::saveEnvironment(const QProcessEnvironment& systemEnvironment, const QStringList& variables, QProcessEnvironment& env)
+  ///
+  /// \sa ctkAppLauncherEnvironment::environment(int requestedLevel), ctkAppLauncherEnvironment::saveEnvironment(const QProcessEnvironment& systemEnvironment, const QStringList& variables, QProcessEnvironment& env)
+  /// \sa ctkAppLauncherSettings::envVars(), ctkAppLauncherSettings::pathsEnvVars()
   void buildEnvironment(QProcessEnvironment&);
 
   QString invalidSettingsMessage()const;
