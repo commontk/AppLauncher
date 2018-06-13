9b3a4e19cdba47b322f2ce6e68c12ac35ec01e09
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-06-13 02:43:54 -0400

    ctkAppLauncher: Only consider executable files when searching PATH. Fixes #57

--- Testing/Launcher/AppLauncher-Param-TestLauncherLaunch-SearchPATH.cmake
+++ Testing/Launcher/AppLauncher-Param-TestLauncherLaunch-SearchPATH.cmake
@@ -2,13 +2,37 @@
 include(${TEST_SOURCE_DIR}/AppLauncherTestMacros.cmake)
 include(${TEST_BINARY_DIR}/AppLauncherTestPrerequisites.cmake)
 
+# Create a directory named like the "application to launch" to
+# test that the launcher ignores it when searching for the executable.
+set(path_with_same_name_directory "${application_dir}/same_name_directory")
+file(MAKE_DIRECTORY "${path_with_same_name_directory}/${application_name}")
+
+# Create a non-executable file named like the "application to launch" to
+# test that the launcher ignores it when searching for the executable.
+set(path_with_same_name_nonexecutable_file "${application_dir}/same_name_nonexecutable_file")
+file(MAKE_DIRECTORY "${path_with_same_name_nonexecutable_file}")
+file(WRITE "${path_with_same_name_nonexecutable_file}/${application_name}")
+
+set(path_with_working_app ${application_dir})
+if(UNIX)
+  set(path_with_same_name_symlink "${application_dir}/same_name_symlink")
+  file(MAKE_DIRECTORY "${path_with_same_name_symlink}")
+  execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink
+    ${application} ${path_with_same_name_symlink}/${application_name})
+  set(path_with_working_app ${path_with_same_name_symlink})
+endif()
+
+
+
 # --------------------------------------------------------------------------
 # Configure settings file
 file(WRITE "${launcher}LauncherSettings.ini" "
 
 [Paths]
-1\\path=${application_dir}
-size=1
+1\\path=${path_with_same_name_directory}
+2\\path=${path_with_same_name_nonexecutable_file}
+3\\path=${path_with_working_app}
+size=3
 
 [LibraryPaths]
 1\\path=${library_path}
@@ -41,3 +65,9 @@ if("${current_msg}" STREQUAL "")
                       "  current_msg:${ov}")
 endif()
 
+# Clean
+file(REMOVE_RECURSE "${path_with_same_name_directory}")
+file(REMOVE_RECURSE "${path_with_same_name_nonexecutable_file}")
+if(UNIX)
+  file(REMOVE_RECURSE "${path_with_same_name_symlink}")
+endif()
