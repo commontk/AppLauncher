commontk/AppLauncher

commit 36e8c12418e53f68ae5ce04abdbb3ea604f76a0c

Don't instantiate QApplication if no splash screen is used
Instantiating a QCoreApplication instead allows the launcher to be run on
a system with no DISPLAY.
It is necessary to parse the arguments and ini settings before the
application can be instantiated.
Fixes AppLauncherTestNoSplashScreen.

Closes #13

Base/ctkAppLauncher.cpp
@@ -65,6 +65,15 @@ void ctkAppLauncherInternal::reportInfo(const QString& msg)
 }
 
 // --------------------------------------------------------------------------
+void ctkAppLauncherInternal::exit(int exitCode)
+{
+  if (this->Application)
+    {
+    this->Application->exit(exitCode);
+    }
+}
+
+// --------------------------------------------------------------------------
 bool ctkAppLauncherInternal::processSplashPathArgument()
 {
   if (this->LauncherSplashImagePath.isEmpty())
@@ -486,7 +495,7 @@ void ctkAppLauncherInternal::runProcess()
     {
     this->Process.startDetached(
       this->ApplicationToLaunch, this->ApplicationToLaunchArguments);
-    this->Application->quit();
+    this->exit(EXIT_SUCCESS);
     }
 }
 
@@ -501,14 +510,14 @@ void ctkAppLauncherInternal::applicationFinished(int exitCode, QProcess::ExitSta
 {
   if (exitStatus == QProcess::NormalExit)
     {
-    this->Application->exit(exitCode);
+    this->exit(exitCode);
     }
   else if (exitStatus == QProcess::CrashExit)
     {
     this->reportError(
       QString("[%1] exit abnormally - Report the problem.").
         arg(this->ApplicationToLaunch));
-    this->Application->exit(EXIT_FAILURE);
+    this->exit(EXIT_FAILURE);
     }
 }
 
@@ -523,12 +532,18 @@ void ctkAppLauncherInternal::applicationStarted()
 // ctkAppLauncher methods
 
 // --------------------------------------------------------------------------
-ctkAppLauncher::ctkAppLauncher(const QCoreApplication& application, QObject* parentObject):
-  Superclass(parentObject)
+ctkAppLauncher::ctkAppLauncher(const QCoreApplication& application, QObject* parentObject)
+  : Superclass(parentObject)
 {
   this->Internal = new ctkAppLauncherInternal();
-  this->Internal->Application = application.instance();
-  this->Internal->Arguments = application.instance()->arguments();
+  this->setApplication(application);
+}
+
+// --------------------------------------------------------------------------
+ctkAppLauncher::ctkAppLauncher(QObject* parentObject)
+  : Superclass(parentObject)
+{
+  this->Internal = new ctkAppLauncherInternal();
 }
 
 // --------------------------------------------------------------------------
@@ -589,16 +604,18 @@ void ctkAppLauncher::displayVersion(std::ostream &output)
 }
 
 // --------------------------------------------------------------------------
-bool ctkAppLauncher::initialize()
+bool ctkAppLauncher::initialize(QString launcherFilePath)
 {
   if (this->Internal->Initialized)
     {
-    this->Internal->reportError("AppLauncher already initialized !");
     return true;
     }
 
-  if (!this->Internal->extractLauncherNameAndDir(
-    this->Internal->Application->applicationFilePath()))
+  if (launcherFilePath.isEmpty() && this->Internal->Application)
+    {
+    launcherFilePath = this->Internal->Application->applicationFilePath();
+    }
+  if (!this->Internal->extractLauncherNameAndDir(launcherFilePath))
     {
     return false;
     }
@@ -630,6 +647,16 @@ bool ctkAppLauncher::initialize()
 
   this->Internal->Initialized = true;
   return true;
+}
+
+// --------------------------------------------------------------------------
+void ctkAppLauncher::setApplication(const QCoreApplication& app)
+{
+  this->Internal->Application = app.instance();
+  if (this->Internal->Application)
+    {
+    this->setArguments(this->Internal->Application->arguments());
+    }
 }
 
 // --------------------------------------------------------------------------
@@ -865,6 +892,12 @@ int ctkAppLauncher::splashScreenHideDelayMs()const
 }
 
 // --------------------------------------------------------------------------
+bool ctkAppLauncher::disableSplash() const
+{
+  return this->Internal->disableSplash();
+}
+
+// --------------------------------------------------------------------------
 bool ctkAppLauncher::verbose()const
 {
   return this->Internal->verbose();
@@ -878,12 +911,14 @@ void ctkAppLauncher::startApplication()
     return;
     }
 
-  this->Internal->SplashPixmap.load(this->splashImagePath());
-  this->Internal->SplashScreen =
-      QSharedPointer<QSplashScreen>(new QSplashScreen(this->Internal->SplashPixmap, Qt::WindowStaysOnTopHint));
+  this->Internal->reportInfo(
+    QString("DisableSplash [%1]").arg(this->Internal->disableSplash()));
   if (!this->Internal->disableSplash())
     {
-    this->Internal->reportInfo(QString("DisableSplash [%1]").arg(this->Internal->disableSplash()));
+    this->Internal->SplashPixmap.reset(
+      new QPixmap(this->splashImagePath()));
+    this->Internal->SplashScreen = QSharedPointer<QSplashScreen>(
+      new QSplashScreen(*this->Internal->SplashPixmap.data(), Qt::WindowStaysOnTopHint));
     this->Internal->SplashScreen->show();
     }
 
@@ -931,12 +966,12 @@ void ctkAppLauncher::generateTemplate()
 }
 
 // --------------------------------------------------------------------------
-void ctkAppLauncher::startLauncher()
+bool ctkAppLauncher::configure()
 {
   if (!this->initialize())
     {
-    this->Internal->Application->exit(EXIT_FAILURE);
-    return;
+    this->Internal->exit(EXIT_FAILURE);
+    return false;
     }
 
   QString settingFileName = this->findSettingFile();
@@ -950,13 +985,13 @@ void ctkAppLauncher::startLauncher()
       {
       this->Internal->reportError(this->Internal->invalidSettingsMessage());
       }
-    this->Internal->Application->exit(EXIT_FAILURE);
-    return;
+    this->Internal->exit(EXIT_FAILURE);
+    return false;
     }
   else if (status == ctkAppLauncher::ExitWithSuccess)
     {
-    this->Internal->Application->exit(EXIT_SUCCESS);
-    return;
+    this->Internal->exit(EXIT_SUCCESS);
+    return false;
     }
 
   // Append 'unparsed arguments' to list of arguments that will be used to start the application.
@@ -973,6 +1008,16 @@ void ctkAppLauncher::startLauncher()
     }
 
   this->Internal->ApplicationToLaunchArguments.append(unparsedArguments);
+  return true;
+}
 
+// --------------------------------------------------------------------------
+void ctkAppLauncher::startLauncher()
+{
+  bool res = this->configure();
+  if (!res)
+    {
+    return;
+    }
   this->startApplication();
 }

Base/ctkAppLauncher.h
@@ -20,6 +20,7 @@ public:
   typedef ctkAppLauncher Self;
   typedef QObject Superclass;
   ctkAppLauncher(const QCoreApplication& application, QObject* parentObject = 0);
+  ctkAppLauncher(QObject* parentObject = 0);
   ~ctkAppLauncher();
 
   enum ProcessArgumentsStatus
@@ -38,12 +39,29 @@ public:
   /// Display version information string on standard output
   void displayVersion(std::ostream &output = std::cout);
 
-  /// Initialize
-  bool initialize();
+  /// Initialize launcher by extracting associated name and directory, and
+  /// configuring the command line parser.
+  /// \a launcherFilePath is the path of the launcher used to extract its
+  /// name and directory. If empty, the application file path is used instead.
+  /// Return false if it fails to initialize the arguments.
+  /// \sa setArguments(), configure()
+  bool initialize(QString launcherFilePath = QString());
+
+  /// Read command line arguments to configure launcher behavior.
+  /// Configure the AppLauncher by first initializing the arguments (see
+  /// initialize()) and then read settings from ini file and parse arguments
+  /// arguments from command line.
+  /// Return true if the application must be launched, false otherwise.
+  /// \sa initialize(), setArguments(), startLauncher()
+  bool configure();
+
+  /// Set the launcher application and its arguments
+  /// \sa setArguments()
+  void setApplication(const QCoreApplication& app);
 
   /// Set/Get list of arguments that will be passed to the program to launch.
   /// By default, \a arguments associated with the \a application are used.
-  /// \sa ctkAppLauncher(), QCoreApplication::arguments()
+  /// \sa ctkAppLauncher(), QCoreApplication::arguments(), setApplication()
   QStringList arguments()const;
   void setArguments(const QStringList& args);
 
@@ -86,6 +104,11 @@ public:
   /// \brief Return the delay in ms before the launcher hides the splashscreen.
   /// The delay is applied after the application finishes to start.
   int splashScreenHideDelayMs()const;
+
+  /// \brief Return true if the splash-screen should not be visible when
+  /// starting the launched application, false otherwise.
+  /// false by default.
+  bool disableSplash()const;
 
   /// Get verbose flag
   /// If True, print to standard output information associated with the parameters

Base/ctkAppLauncher_p.h
@@ -30,6 +30,10 @@ public:
   /// Display status on Standard Output
   void reportInfo(const QString& msg);
 
+  /// Exit the application if the application has been set.
+  /// \sa ctkAppLauncher::setApplication
+  void exit(int exitCode);
+
   enum SettingsType
     {
     RegularSettings = 0,
@@ -81,6 +85,7 @@ public:
   QStringList ApplicationToLaunchArguments;
   QString     LauncherSplashImagePath;
   int         LauncherSplashScreenHideDelayMs;
+  bool        LauncherNoSplashScreen;
 
   /// Variable used internally
   QStringList                     Arguments;
@@ -91,6 +96,7 @@ public:
   QString                         DefaultApplicationToLaunch;
   QString                         DefaultLauncherSplashImagePath;
   int                             DefaultLauncherSplashScreenHideDelayMs;
+  bool                            DefaultLauncherNoSplashScreen;
   QString                         DefaultApplicationToLaunchArguments;
   QString                         LauncherName;
   QString                         LauncherDir;
@@ -108,7 +114,7 @@ public:
   QString                         PathSep;
   QString                         LibraryPathVariableName;
   QSharedPointer<QSplashScreen>   SplashScreen;
-  QPixmap                         SplashPixmap;
+  QScopedPointer<QPixmap>         SplashPixmap;
 
   /// Extra 'application to launch'
   QString                                          ExtraApplicationToLaunchLongArgument;

Main.cpp
@@ -1,9 +1,11 @@
 
 // Qt includes
 #include <QApplication>
+#include <QDebug>
+#include <QDir>
+#include <QFileInfo>
 #include <QStringList>
 #include <QTimer>
-#include <QDebug>
 
 // CTK includes
 #include "ctkAppArguments.h"
@@ -67,18 +69,44 @@ int appLauncherMain(int argc, char** argv)
 #endif
         );
 
-  QApplication app(appArguments.argumentCount(ctkAppArguments::ARG_REGULAR_LIST),
-                   appArguments.argumentValues(ctkAppArguments::ARG_REGULAR_LIST));
-
+  QFileInfo launcherFile(QDir::current(), QString(argv[0]));
   // Initialize resources in static libs
   Q_INIT_RESOURCE(CTKAppLauncherBase);
 
-  ctkAppLauncher appLauncher(app);
-  appLauncher.setArguments(appArguments.arguments());
+  QScopedPointer<ctkAppLauncher> appLauncher(new ctkAppLauncher);
+  appLauncher->setArguments(appArguments.arguments());
+  bool exec = appLauncher->initialize(launcherFile.absoluteFilePath());
+  exec = appLauncher->configure() && exec;
 
-  QTimer::singleShot(0, &appLauncher, SLOT(startLauncher()));
+  if (!exec)
+    {
+    return EXIT_SUCCESS;
+    }
 
-  return app.exec();
+  QScopedPointer<QCoreApplication> app;
+  if (appLauncher->disableSplash())
+    {
+    app.reset(new QCoreApplication(
+                appArguments.argumentCount(ctkAppArguments::ARG_REGULAR_LIST),
+                appArguments.argumentValues(ctkAppArguments::ARG_REGULAR_LIST)));
+    }
+  else
+    {
+    app.reset(new QApplication(
+                appArguments.argumentCount(ctkAppArguments::ARG_REGULAR_LIST),
+                appArguments.argumentValues(ctkAppArguments::ARG_REGULAR_LIST)));
+    }
+  appLauncher->setApplication(*app.data());
+
+  QTimer::singleShot(0, appLauncher.data(), SLOT(startLauncher()));
+
+  int res = app->exec();
+
+  // Delete application launcher appLauncher before the application app so that
+  // graphical items such as pixmaps, widgets, etc can be released.
+  appLauncher.reset();
+
+  return res;
 }
 
 // --------------------------------------------------------------------------

Testing/Cpp/AppLauncherTestNoSplashScreen.cmake
@@ -2,6 +2,9 @@
 # AppLauncherTestNoSplashScreen
 #
 
+# Disable DISPLAY environment variable to test if the following works when
+# no graphical context can be instantiated (e.g. use QCoreApplication instead
+# of QApplication)
 set(ENV{DISPLAY})
 
 include(${TEST_SOURCE_DIR}/AppLauncherTestMacros.cmake)
@@ -29,7 +32,7 @@ execute_process(
   )
 
 # --------------------------------------------------------------------------
-# Test1 - Check if flag --launcher-version works as expected
+# TestNoSplashScreen - Check if launcher can work in no DISPLAY environment
 set(command ${launcher_exe} --launcher-no-splash )
 execute_process(
   COMMAND ${command}
@@ -42,6 +45,7 @@ execute_process(
 print_command_as_string("${command}")
 
 if(rv)
-  message(FATAL_ERROR "TestNoSplashScreen - failed to start launcher "
-    "with no splash screen if no DISPLAY\n${ev}")
+  message(FATAL_ERROR "TestNoSplashScreen - Failed to start launcher "
+    "using `--launcher-no-splash`. Launcher is expected to run even if DISPLAY "
+    "environment variable is unset.\n${ev}")
 endif()

commit 249be3df83b43dd37334eef6dd2f2d7865ff3864

BUG: Ensure Qt reserved arguments are passed to the launched application
This commit adds a test checking that Qt reserved arguments are effectively
passed to the launched application. It fixes a regression likely introduced
in 36e8c1241 (Don't instantiate QApplication if no splash screen is used).

Base/ctkAppLauncher.cpp
@@ -993,7 +993,7 @@ void ctkAppLauncher::setApplication(const QCoreApplication& app)
 {
   Q_D(ctkAppLauncher);
   d->Application = app.instance();
-  if (d->Application)
+  if (d->Application && d->Arguments.empty())
     {
     this->setArguments(d->Application->arguments());
     }

Testing/Launcher/App4Test/Main.cpp.in
@@ -220,6 +220,36 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
         }
       }
+    else if (argName == "--check-reserved-argument")
+      {
+      std::string expectedArgName2("-widgetcount");
+      int expectedArgCount = 3;
+      if (argc != expectedArgCount)
+        {
+        std::cerr << "Unexpected number of arguments"
+                  << "\n\texpected: " << expectedArgCount
+                  << "\n\t current: " << argc
+                  << std::endl;
+        std::cerr << "Argument passed:" << argName;
+        for(int i=2; i < argc; i++)
+          {
+          std::cerr << " " << argv[i];
+          }
+        std::cerr << std::endl;
+        return EXIT_FAILURE;
+        }
+      std::string argName2(argv[2]);
+      if (argName2 != expectedArgName2)
+        {
+        std::cerr << "Unexpected argument #2:"
+          << "\n\texpected: " << expectedArgName2
+          << "\n\t current: " << argName2
+          << std::endl;
+        display_arguments(std::cerr, argc, argv);
+        return EXIT_FAILURE;
+        }
+      std::cout << expectedArgName2 << " argument passed" << std::endl;
+      }
     }
   else if (argc == 1)
     {

Testing/Launcher/AppLauncher-Param-TestQtReservedArgs.cmake
@@ -18,8 +18,9 @@ size=1
 set(PRINT_COMMAND 0)
 
 # --------------------------------------------------------------------------
-# Make sure the launcher do not consider Qt reserved argument
-set(command ${launcher_exe} -widgetcount --launcher-no-splash)
+# Make sure that (1) the launcher do not consider Qt reserved argument
+# and (2) the launched application consider Qt reserved argument
+set(command ${launcher_exe} --check-reserved-argument -widgetcount)
 execute_process(
   COMMAND ${command}
   WORKING_DIRECTORY ${launcher_binary_dir}
@@ -39,3 +40,6 @@ if("${ev}" MATCHES "Widgets left.*")
   message(FATAL_ERROR "error_variable [${ev}] shouldn't contain 'Widgets left'")
 endif()
 
+if(NOT "${ov}" MATCHES "-widgetcount argument passed")
+  message(FATAL_ERROR "output_variable [${ov}] should contain '-widgetcount argument passed'")
+endif()

Testing/Launcher/CMakeLists.txt
@@ -147,6 +147,7 @@ applauncher_add_test(AppLauncher-Param-TestLauncherTimeout)
 applauncher_add_test(AppLauncher-Param-TestLauncherVerbose)
 applauncher_add_test(AppLauncher-Param-TestLauncherVersion)
 applauncher_add_test(AppLauncher-Param-TestQtReservedArgs)
+set_property(TEST "${testname}" APPEND PROPERTY LABELS "XDisplayRequired")
 
 applauncher_add_test(AppLauncher-Settings-TestApplicationArguments)
 applauncher_add_test(AppLauncher-Settings-TestApplicationPath)