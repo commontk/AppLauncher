b62b687429c01386fd0fdd5bda5fc7f31d590a9c
Hans Johnson <hans-johnson@uiowa.edu>
2025-06-19 15:20:12 -0500

    COMP: Update CMake minimum required version from 3.0 to 3.16.3

--- Testing/Launcher/AppLauncher-TestSaveLauncherEnvironment.cmake
+++ Testing/Launcher/AppLauncher-TestSaveLauncherEnvironment.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.0)
+cmake_minimum_required(VERSION 3.16.3)
 
 # --------------------------------------------------------------------------
 include(${TEST_SOURCE_DIR}/AppLauncherTestMacros.cmake)
