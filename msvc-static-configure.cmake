32925876e50ba2ee89d82a013da1bc85def11ae8
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2025-07-24 21:53:28 -0400

    chore(msvc-static-configure.cmake): Update FindVcvars from v1.6 to v1.7
    v1.7:
    
    * Add support for msvc version 1940 (Visual Studio 2022)
    * Anticipate future VS 2022 releases up to msvc 1950
    * docs: Add changelog insertion placeholder and streamline release instructions

--- msvc-static-configure.cmake
+++ msvc-static-configure.cmake
@@ -42,8 +42,8 @@ if(APPLAUNCHER_USE_NINJA)
 
   # Download FindVcvars.cmake
   set(dest_file "${CMAKE_CURRENT_BINARY_DIR}/FindVcvars.cmake")
-  set(expected_hash "0fd1104a5e0f91f89b64dd700ce57ab9f7d7356fcabe9c895e62aca32386008e")
-  set(url "https://raw.githubusercontent.com/scikit-build/cmake-FindVcvars/v1.6/FindVcvars.cmake")
+  set(expected_hash "a3097481cedf3d7cdbdd3a20551da54e1627fe16fbe9aa18b9c7a1c70b50274e")
+  set(url "https://raw.githubusercontent.com/scikit-build/cmake-FindVcvars/v1.7/FindVcvars.cmake")
   if(NOT EXISTS ${dest_file})
     file(DOWNLOAD ${url} ${dest_file} EXPECTED_HASH SHA256=${expected_hash})
   else()
